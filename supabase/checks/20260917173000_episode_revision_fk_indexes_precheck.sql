fce0116815368be681b78829849b0726f97c0b19
bingohooah888-ai <bingo.hooah.888@gmail.com>
2026-09-17 17:43:24 +0900

    Remove superseded FK index precheck filename

--- supabase/checks/20260917173000_episode_revision_fk_indexes_precheck.sql
+++
@@ -1,9 +0,0 @@
--- Read-only precheck for 20260917173000_episode_revision_fk_indexes.sql
-
-do $$
-begin
-  if to_regclass('public.episode_revisions') is null then
-    raise exception 'public.episode_revisions is missing';
-  end if;
-end;
-$$;
