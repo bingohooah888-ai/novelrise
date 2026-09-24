48cc6b69ae1be1da8ac52ee206aaa02d649eb960
bingohooah888-ai <bingo.hooah.888@gmail.com>
2026-09-25 08:17:06 +0900

    style: apply Prettier arrow formatting

--- tests/author-studio-page-unification.test.mjs
+++ tests/author-studio-page-unification.test.mjs
@@ -11,7 +11,7 @@ const pagePaths = [
   'account-settings.html'
 ];
 
-const pages = await Promise.all(pagePaths.map(path => readFile(path, 'utf8')));
+const pages = await Promise.all(\n  pagePaths.map((path) => readFile(path, 'utf8'))\n);
 const styles = await readFile('novelight-author-studio-pages.css', 'utf8');
 
 test('target creator pages share the Author Studio presentation layer', () => {
